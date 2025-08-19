2b4b48bf43a772e782409ee34183a20c0095aebe
ItencY <108655765+ItencY@users.noreply.github.com>
2025-08-19 20:22:05 +0300

    Delete dist directory

--- dist/command_help.js
+++
@@ -1,6 +0,0 @@
-export function commandHelp() {
-    console.log("Welcome to the Pokedex!");
-    console.log("Usage:");
-    console.log("help: Displays a help message");
-    console.log("exit: Exit the Pokedex");
-}
